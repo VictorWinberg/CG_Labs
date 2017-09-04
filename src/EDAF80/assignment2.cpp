6d4d3fdcd2628c8f7a5badbf7058b09b295e8daa
VictorWinberg <dat14vwi@student.lu.se>
2017-09-04 12:31:23 +0200

    [Assignment2] - Sphere progress

--- src/EDAF80/assignment2.cpp
+++ src/EDAF80/assignment2.cpp
@@ -67,6 +67,7 @@ edaf80::Assignment2::run()
 	// Load the sphere geometry
 //	auto const shape = parametric_shapes::createCircleRing(4u, 60u, 1.0f, 2.0f);
 	auto const shape = parametric_shapes::createQuad(4u, 2u);
+//	auto const shape = parametric_shapes::createSphere(60u, 0, 2.0f);
 	if (shape.vao == 0u)
 		return;
 
